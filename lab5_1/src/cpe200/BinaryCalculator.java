3d836c37b8887309a1093ba3e2dcb7729f2698e7
A550J <nice.dark@hotmail.com>
2016-09-30 17:24:58 +0700

    Finished

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -2,37 +2,83 @@ package cpe200;
 
 
 public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+    private IOperand firstOperand;
+    private IOperand secondOperand;
+    double sum=0,first=0,second=0;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(IOperand operand) {
+        first = new Double(operand.getOperand());
+
     }
 
     public void setSecondOperand(IOperand operand) {
+        second = new Double(operand.getOperand());
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if (first < 0 || second < 0){
+            throw new RuntimeException("Negative number");
+        }
+        sum = (first+second);
+        String sums =  Double.toString(sum);
+        return sums = sums.indexOf(".") < 0 ? sums : sums.replaceAll("0*$", "").replaceAll("\\.$", "");
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if (first < 0 || second < 0){
+            throw new RuntimeException("Negative number");
+        }
+        sum=(first-second);
+        String sums =  Double.toString(sum);
+        sums = String.format("%.5f", sum);
+        return sums = sums.indexOf(".") < 0 ? sums : sums.replaceAll("0*$", "").replaceAll("\\.$", "");
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if (first < 0 || second < 0){
+            throw new RuntimeException("Negative number");
+        }
+        sum=(first*second);
+        String sums =  Double.toString(sum);
+        return sums = sums.indexOf(".") < 0 ? sums : sums.replaceAll("0*$", "").replaceAll("\\.$", "");
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if (second==0)
+        {
+            throw new ArithmeticException("divide by zero");
+        }
+        if (first < 0 || second < 0){
+            throw new RuntimeException("Negative number");
+        }
+
+        sum=(first/second);
+        String sums =  Double.toString(sum);
+        sums = String.format("%.5f", sum);
+        return sums = sums.indexOf(".") < 0 ? sums : sums.replaceAll("0*$", "").replaceAll("\\.$", "");
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if (first < 0 || second < 0){
+            throw new RuntimeException("Negative number");
+        }
+        sum = 1 ;
+        for (int i = 1; i <= second; i++) {
+            sum = sum*first;
+        }
+        String sums =  Double.toString(sum);
+        return sums = sums.indexOf(".") < 0 ? sums : sums.replaceAll("0*$", "").replaceAll("\\.$", "");
     }
 
+    public IOperand getFirstOperand() {
+        return firstOperand;
+    }
+
+    public IOperand getSecondOperand() {
+        return secondOperand;
+    }
 }
--- lab5_1/src/cpe200/IntegerOperand.java
+++ lab5_1/src/cpe200/IntegerOperand.java
@@ -4,13 +4,17 @@ package cpe200;
  * Created by pruet on 26/9/2559.
  */
 public class IntegerOperand implements IOperand {
+    private String operand;
 
     public IntegerOperand(int i) {
+        this.setOperand((Integer.toString(i)));
 
     }
 
     @Override
     public String getOperand() {
-        return null;
+        return operand;
     }
+    public String setOperand(String operand) { return this.operand = operand;}
 }
+
