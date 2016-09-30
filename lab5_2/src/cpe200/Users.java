661ba9bb5cf087a44c7883166a3cdbe46cc2ebd4
nanearnano <nanearnano@gmail.com>
2016-09-30 17:25:43 +0700

    6-2

--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -9,42 +9,66 @@ import java.util.ArrayList;
 public class Users {
     public ArrayList<IUser> userList;
 
+    public  Users(){
+        userList=new ArrayList<>();
+    }
+
     public void addUser(IUser user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User user = new User();
+        user.setUserName(userName);
+        user.setPassword(password);
+        userList.add(user);
     }
 
     public void deleteUser(IUser user)
     {
+        if(userList.size()==0){
+            throw new RuntimeException("Empty List!");
+        }
+        userList.remove(user);
 
     }
 
     public boolean exists(IUser user)
     {
-        return false;
+        return userList.contains(user);
     }
 
     public boolean usernameExists(String username)
     {
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getUserName().equals(username)) {
+                return true;
+
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public IUser getUserByUsername(String userName)
     {
+        for (int i = 0; i < userList.size(); i++) {
+        if (userList.get(i).getUserName().equals(userName)) {
+            return userList.get(i);
+        }
+    }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public IUser[] getUserArray()
     {
-        return null;
+        return userList.toArray(new User[userList.size()]);
     }
 }
