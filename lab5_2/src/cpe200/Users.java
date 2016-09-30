eab0c858405b5d0b7504abae2c0d895dee0033b5
purrirorin <cc-aaa-tt@hotmail.com>
2016-09-30 18:27:07 +0700

    i can do only this T T

--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -8,43 +8,54 @@ import java.util.ArrayList;
  */
 public class Users {
     public ArrayList<IUser> userList;
+    public Users(){
+               userList = new ArrayList<IUser>();
+           }
 
     public void addUser(IUser user)
     {
+        userList.add(user);
     }
 
-    public void addUser(String userName, String password)
-    {
+        public void addUser(String userName, String password) {
+               IUser temp = new User(userName,password);
+                userList.add(temp);
+
     }
 
     public void deleteUser(IUser user)
     {
-
+        userList.remove(userList.indexOf(user));
     }
 
     public boolean exists(IUser user)
     {
+        return userList.contains(user);
+    }
+
+    public boolean usernameExists(String username){
+
         return false;
     }
 
-    public boolean usernameExists(String username)
-    {
-        return false;
-    }
 
     /* This method should return null when the user with username is not in the list */
     public IUser getUserByUsername(String userName)
     {
+        for (IUser i : userList){
+                       if (i.getUserName().equals(userName)) return i;
+                    }
+
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public IUser[] getUserArray()
     {
-        return null;
+        return userList.toArray(new IUser[1]);
     }
 }
