45444eeaccafd8ff96de2c7bdba7f27acc84f5f1
OomDuanghong <d.nuttinee@hotmail.com>
2016-09-30 17:01:17 +0700

    Lab 1,2 : finished

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,4 +4,44 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User implements IUser {
+    private String userName;
+    private String password;
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String setUserName(String u) {
+        String OldName;
+        String Ncheck = "[A-Za-z][A-Za-z0-9]{7,}";
+        if(u == null){
+            return null;
+        }
+        else if(!u.matches(Ncheck)){
+            throw new RuntimeException();
+        }
+        else{
+            OldName = userName;
+            userName = u;
+            return OldName;
+        }
+    }
+
+    @Override
+    public int setPassword(String p) {
+        String PCheck = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9]{12,})";
+        if (!p.matches(PCheck)){
+            throw new RuntimeException();
+        }
+        else
+            password = p;
+        return password.length();
+    }
 }
