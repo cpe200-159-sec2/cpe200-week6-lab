16f6f1e2f7010f801810e25c119ca24e10ed45d0
Ngamsai <ngamsiri.bm@gmail.com>
2016-09-30 16:48:28 +0700

    Complete Lab 6.1

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,38 +1,79 @@
 package cpe200;
 
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+    private IOperand firstOperand;
+    private IOperand secondOperand;
+    public BigDecimal first , second , result;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(IOperand operand) {
+        first = new BigDecimal(operand.getOperand());
     }
 
     public void setSecondOperand(IOperand operand) {
+        second = new BigDecimal(operand.getOperand());
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if (first.intValue()<0||second.intValue()<0){
+            throw new RuntimeException("Negative value");
+        }else {
+            result = first.add(second).stripTrailingZeros();
+            return result.toString();
+        }
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if (first.intValue()<0||second.intValue()<0){
+            throw new RuntimeException("Negative value");
+        } else {
+            result = first.subtract(second).stripTrailingZeros();
+            return result.toString();
+        }
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if (first.intValue()<0||second.intValue()<0){
+            throw new RuntimeException("Negative value");
+        } else {
+            result = first.multiply(second).stripTrailingZeros();
+            return result.toString();
+        }
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if (second.equals(0)){
+            throw new RuntimeException("Divide by Zero");
+        } else if (first.intValue()<0||second.intValue()<0){
+            throw new RuntimeException("Negative value");
+        } else {
+            result = first.divide(second,5,BigDecimal.ROUND_UP).stripTrailingZeros();
+            return result.toString();
+        }
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if (first.intValue()<0||second.intValue()<0){
+            throw new RuntimeException("Negative value");
+        }
+        result = first.pow(second.intValue()).stripTrailingZeros();
+        return result.toString();
+    }
+
+    public IOperand getFirstOperand(){
+        return firstOperand;
+    }
+
+    public IOperand getSecondOperand(){
+        return getSecondOperand();
     }
 
 }
