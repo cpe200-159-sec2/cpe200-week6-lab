ab16131405651160036bc8bc90487944435f67a5
supakarn39 <supakarn39@hotmail.com>
2016-09-30 17:38:31 +0700

    Completed!

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,4 +4,39 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User implements IUser {
+    private String userName;
+    private String password;
+
+    @Override
+    public String setUserName(String name) throws RuntimeException {
+        if(name==null){
+            return null;
+        }else if (name.length()>=8&&name.matches("^[A-Za-z][A-Za-z0-9]+$")){
+            String temp = this.userName;
+            this.userName = name;
+            return temp;
+        }else {
+            throw new RuntimeException("Wrong input");
+        }
+    }
+
+    @Override
+    public int setPassword(String name) throws RuntimeException {
+        if(name.length()>=12&&name.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[A-Za-z0-9]+")){
+            this.password = name;
+            return this.password.length();
+        }else {
+            throw new RuntimeException("Wrong input");
+        }
+    }
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
 }
--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -7,44 +7,79 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<IUser> userList;
+    private ArrayList<IUser> userList;
+
+    public Users(){
+        setUserList(new ArrayList<IUser>());
+    }
 
     public void addUser(IUser user)
     {
+        getUserList().add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User user = new User();
+        user.setUserName(userName);
+        user.setPassword(password);
+        getUserList().add(user);
     }
 
-    public void deleteUser(IUser user)
+    public void deleteUser(IUser user) throws RuntimeException
     {
-
+        if(exists(user)){
+            getUserList().remove(user);
+        }else {
+            throw new RuntimeException("User Not Found!");
+        }
     }
 
     public boolean exists(IUser user)
     {
-        return false;
+        if(getUserList().contains(user)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public boolean usernameExists(String username)
     {
+        for(int i = 0; i< getUserList().size(); i++){
+            if(getUserList().get(i).getUserName().equals(username)){
+                return true;
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public IUser getUserByUsername(String userName)
     {
+        for(int i = 0; i< getUserList().size(); i++){
+            if(getUserList().get(i).getUserName().equals(userName)){
+                return getUserList().get(i);
+            }
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return getUserList().size();
     }
 
     public IUser[] getUserArray()
     {
-        return null;
+        return getUserList().toArray(new User[getUserList().size()]);
+    }
+
+    public ArrayList<IUser> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(ArrayList<IUser> userList) {
+        this.userList = userList;
     }
 }
