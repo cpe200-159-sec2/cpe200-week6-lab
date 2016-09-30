ab20d2583de83cc6f365024778dfe2ea4418a264
gelaro <beelzebub_151@hotmail.com>
2016-09-30 21:37:24 +0700

    เสร็จซะที~~~~~~~~

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,38 +1,69 @@
 package cpe200;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
 
     public BinaryCalculator() {
+        firstOperand = new BigDecimal(0);
+        secondOperand = new BigDecimal(0);
     }
 
     public void setFirstOperand(IOperand operand) {
+        firstOperand = new BigDecimal(operand.getOperand());
+    }
+
+    public BigDecimal getFirstOperand(){
+        return firstOperand;
     }
 
     public void setSecondOperand(IOperand operand) {
+        secondOperand = new BigDecimal(operand.getOperand());
+    }
+
+    public BigDecimal getSecondOperand(){
+        return secondOperand;
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0 || getSecondOperand().intValue()<0){
+            throw new RuntimeException("ERROR");
+        }
+        return getFirstOperand().add(getSecondOperand()).stripTrailingZeros().toString();
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0 || getSecondOperand().intValue()<0){
+            throw new RuntimeException("ERROR");
+        }
+        return  getFirstOperand().subtract(getSecondOperand()).stripTrailingZeros().toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0 || getSecondOperand().intValue()<0){
+            throw new RuntimeException("ERROR");
+        }
+        return  getFirstOperand().multiply(getSecondOperand()).stripTrailingZeros().toString();
+
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0 || getSecondOperand().intValue()<0){
+            throw new RuntimeException("ERROR");
+        }
+        return getFirstOperand().divide(getSecondOperand(), 5, BigDecimal.ROUND_CEILING).stripTrailingZeros().toString();
+
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0 || getSecondOperand().intValue()<0){
+            throw new RuntimeException("ERROR");
+        }
+        return  getFirstOperand().stripTrailingZeros().pow(getSecondOperand().intValue()).stripTrailingZeros().toString();
     }
 
 }
--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,4 +4,51 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User implements IUser {
+    private String userName;
+    private String password;
+
+    public  User(){
+    }
+
+    public User(String userName,String password){
+        this.setUserName(userName);
+        this.setPassword(password);
+    }
+
+    @Override
+    public String getUserName(){
+        return this.userName;
+    }
+
+    @Override
+    public String getPassword(){
+        return this.password;
+    }
+
+    @Override
+    public String setUserName(String name){
+        String oldname;
+        String rx = "[A-Za-z][0-9A-Za-z]{7,}";
+        if(name == null) return null;
+        if(name.matches(rx))
+        {
+            oldname = userName;
+            this.userName = name;
+            return oldname;
+        }
+        else throw new RuntimeException();
+
+    }
+
+    @Override
+    public int setPassword(String name){
+        String rx = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{12,}";
+        if ( name.matches((rx)))
+        {
+            this.password = name;
+            return password.length();
+        }
+        else throw new RuntimeException();
+
+    }
 }
