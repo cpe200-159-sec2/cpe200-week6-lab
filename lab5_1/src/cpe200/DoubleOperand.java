16b93edaec867cd763e28c188416e8ec27a5a5c7
Pair <roybura.b@gmail.com>
2016-10-04 11:21:33 +0700

    finished

--- lab5_1/src/cpe200/DoubleOperand.java
+++ lab5_1/src/cpe200/DoubleOperand.java
@@ -4,13 +4,13 @@ package cpe200;
  * Created by pruet on 26/9/2559.
  */
 public class DoubleOperand implements IOperand{
-
+    private String operand;
     public DoubleOperand(Double d) {
-
+        operand = d.toString();
     }
 
     @Override
     public String getOperand() {
-        return null;
+        return operand;
     }
 }
--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,4 +4,44 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User implements IUser {
+    private String userName = " ";
+    private String password = " ";
+    private static String oldusername = null;
+    private static final String CHECKusername = "^[A-Za-z][A-Za-z0-9]{7,}$";
+
+    public User() {
+        }
+
+    public String setUsername(String name) {
+        this.oldusername = this.userName ;
+        if (name == null){
+            return null;
+        }
+        if (name.matches(CHECKusername)){
+            this.userName = name;
+        } else {
+            throw new RuntimeException("Incorrect name.");
+        } return this.oldusername;
+    }
+
+    public int setPassword(String name){
+        if((name.length()>=12)&&(name.matches(".*[a-z]+.*"))&&(name.matches(".*[A-Z]+.*"))&&(name.matches(".*[0-9]+.*"))){
+            this.password = name;
+        }else {
+            throw new RuntimeException("Incorrect password.");
+        } return password.length();
+    }
+
+    public String getUsername(){
+        return this.userName;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    @Override
+    public String setUserName(String u) {
+        return null;
+    }
 }
