1bc9f8c89510dde5173cb14de4091eee8219b8a6
C475L4V3 <satethapan.naowasate@gmail.com>
2016-10-10 01:23:10 +0700

    Finish #SorryForLate

--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -7,44 +7,76 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<IUser> userList;
+    private ArrayList<IUser> userList;
 
-    public void addUser(IUser user)
-    {
+    public Users(){
+        setUserList(new ArrayList<IUser>());
     }
 
-    public void addUser(String userName, String password)
-    {
+    public void addUser(IUser user) {
+        getUserList().add(user) ;
     }
 
-    public void deleteUser(IUser user)
-    {
+    public void addUser(String userName, String password) {
+        User user = new User();
+        user.setUserName(userName) ;
+        user.setPassword(password) ;
+        getUserList().add(user) ;
 
+    }
+
+    public void deleteUser(IUser user) {
+        if(count()==0) throw new RuntimeException("") ;
+        getUserList().remove(user) ;
     }
 
     public boolean exists(IUser user)
     {
-        return false;
+        if(getUserList().contains(user)){
+            return true ;
+        }
+        else {
+            return false ;
+        }
     }
 
     public boolean usernameExists(String username)
     {
-        return false;
+        for(int i = 0; i< getUserList().size() ; i++){
+            if(getUserList().get(i).equals(username)){
+                return true ;
+            }
+
+        }
+        throw new RuntimeException("ERROR") ;
     }
 
     /* This method should return null when the user with username is not in the list */
     public IUser getUserByUsername(String userName)
     {
-        return null;
+        for(int i = 0; i< getUserList().size() ; i++) {
+            if (getUserList().get(i).getUserName().equals(userName)) {
+                return getUserList().get(i);
+            }
+        }
+        throw new RuntimeException("ERROR") ;
     }
 
     public int count()
     {
-        return 0;
+        return getUserList().size();
     }
 
     public IUser[] getUserArray()
     {
-        return null;
+        return getUserList().toArray(new User[getUserList().size()]) ;
+    }
+
+    public ArrayList<IUser> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(ArrayList<IUser> userList) {
+        this.userList = userList;
     }
 }
